66b76a77ad798d57e3ab2b2dca2c3d525f0b0f82
stanisharul <stanisharul@gmail.com>
2019-11-08 10:02:38 +0530

    Datatypes: Includes Hexa Number

--- index.ts
+++ index.ts
@@ -1,21 +1,26 @@
 // Import stylesheets
 import "./style.css";
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById("app");
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
-
 class Typy {
   private _language: string;
+  private _guy: number;
   audience: string;
-  constructor(language: string) {
+  constructor(language: string, guy?: number) {
     this._language = language;
+    this._guy = guy;
   }
-  get language():string{
-    return this._language
+  get language(): string {
+    return this._language;
+  }
+  get guy(): number {
+    return this._guy;
   }
 }
 
-let type = new Typy("TypeScript");
+let type: Typy = new Typy("TypeScript", 0x813D);
 console.log(type);
 console.log(type.language);
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById("app");
+appDiv.innerHTML = `<h1>${type.language} Starter by ${type.guy}</h1>`;
